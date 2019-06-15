6a54a39ba6065bbcf35b526285fdf6066bdd1828
Iago <iagofalcom@hotmail.com>
2019-06-15 17:25:34 -0300

    Adicionando os Campos de Texto e Imagens e Mascaras de Texto
    * criando o _nomeController
    * criando o _emailController
    * criando o _telefoneMask
    
    * Criando mascaras de Texto

--- lib/ui/contato_page.dart
+++ lib/ui/contato_page.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:agenda_de_contatos/helpers/contato_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
 
 class ContatoPage extends StatefulWidget {
 
@@ -13,6 +16,13 @@ class ContatoPage extends StatefulWidget {
 
 class _ContatoPageState extends State<ContatoPage> {
 
+  final _nomeController = TextEditingController();
+  final _emailController = TextEditingController();
+  //final _telefoneController = TextEditingController();
+
+  final _telefoneMask = MaskedTextController(mask: "(00) 00000-0000");
+
+  bool _userEdited = false;
   Contato _editeContato;
 
   @override
@@ -24,6 +34,10 @@ class _ContatoPageState extends State<ContatoPage> {
     }
     else{
       _editeContato = Contato.fromMap(widget.contato.toMap());
+
+      _nomeController.text = _editeContato.nome;
+      _emailController.text = _editeContato.email;
+      _telefoneMask.text = _editeContato.telefone;
     }
   }
 
@@ -41,8 +55,58 @@ class _ContatoPageState extends State<ContatoPage> {
           child: Icon(Icons.save),
         backgroundColor: Colors.red,
       ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: <Widget>[
+            GestureDetector(
+              child: Container(
+                width: 140.0,
+                height: 140.0,
+                foregroundDecoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: _editeContato.img != null ?
+                    FileImage(File(_editeContato.img)):
+                    AssetImage("images/images.png"),
+                  ),
+                ),
+              ),
+            ),
+            TextField(
+              controller: _nomeController,
+              decoration: InputDecoration(labelText: "Nome"),
+              onChanged: (text){
+                _userEdited = true;
+                setState(() {
+                  _editeContato.nome = text;
+                });
+              }),
+            TextField(
+              controller: _emailController,
+                decoration: InputDecoration(labelText: "Email"),
+                onChanged: (text){
+                  _userEdited = true;
+                  _editeContato.email = text;
+                },
+              keyboardType: TextInputType.emailAddress,
+                ),
+            TextField(
+              controller: _telefoneMask,
+                decoration: InputDecoration(labelText: "Telefone"),
+                onChanged: (text){
+                  _userEdited = true;
+                  _editeContato.telefone = text;
+                },
+              keyboardType: TextInputType.phone,
 
 
+                ),
+          ],
+
+
+        ),
+      ),
     );
   }
 }
