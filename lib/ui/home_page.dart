30cee9c4c554c5a5af3bc5cb08fc02478cd9315a
Iago <iagofalcom@hotmail.com>
2019-06-15 22:32:12 -0300

    Adicionado showModalBottomSheet
    * Tela utilizada para adicionar botões flutuantes ao clicar em um contato

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -11,10 +11,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   ContatoHelper helper = ContatoHelper();
 
-  List<Contato> contatos =  List();
+  List<Contato> contatos = List();
 
   @override
   void initState() {
@@ -40,93 +39,159 @@ class _HomePageState extends State<HomePage> {
       ),
       backgroundColor: Colors.white,
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
           _showContatoPage();
         },
         child: Icon(Icons.add),
         backgroundColor: Colors.red,
-        ),
+      ),
       body: ListView.builder(
         padding: EdgeInsets.all(10.0),
         itemCount: contatos.length,
-        itemBuilder: (context, index){
+        itemBuilder: (context, index) {
           return _contatoCard(context, index);
         },
       ),
     );
   }
 
-  Widget _contatoCard(BuildContext context, int index){
+  Widget _contatoCard(BuildContext context, int index) {
     return GestureDetector(
       child: Card(
-        child: Padding(padding: EdgeInsets.all(10.0),
-        child: Row(
-          children: <Widget>[
-            Container(
-              width: 80.0,
-              height: 80.0,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                    image: contatos[index].img != null ?
-                    FileImage(File(contatos[index].img)):
-                        AssetImage("images/images.png"),
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Row(
+            children: <Widget>[
+              Container(
+                width: 80.0,
+                height: 80.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: contatos[index].img != null
+                        ? FileImage(File(contatos[index].img))
+                        : AssetImage("images/images.png"),
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 10.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(contatos[index].nome ?? "",
-                    style: TextStyle(fontSize: 22.0,
-                        fontWeight: FontWeight.bold
+              Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      contatos[index].nome ?? "",
+                      style: TextStyle(
+                          fontSize: 22.0, fontWeight: FontWeight.bold),
                     ),
-                  ),
-                  Text(contatos[index].email ?? "",
-                    style: TextStyle(fontSize: 18.0
+                    Text(
+                      contatos[index].email ?? "",
+                      style: TextStyle(fontSize: 18.0),
                     ),
-                  ),
-                  Text(contatos[index].telefone ?? "",
-                    style: TextStyle(fontSize: 18.0
+                    Text(
+                      contatos[index].telefone ?? "",
+                      style: TextStyle(fontSize: 18.0),
                     ),
-                  ),
-                ],
-              ),
-            )
-          ],
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
-      ),
-      onTap: (){
-        _showContatoPage(contato: contatos[index]);
+      onTap: () {
+        _showOptions(context, index);
       },
     );
   }
-  void _showContatoPage({Contato contato}) async{
-    final recContato =  await Navigator.push(context,
-        MaterialPageRoute(builder: (context) =>
-            ContatoPage(contato: contato
-            )
-        )
-    );
-    if(recContato != null){
-      if(contato != null){
+
+  void _showOptions(BuildContext context, int index) {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return BottomSheet(
+            onClosing: () {},
+            builder: (context) {
+              return Container(
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: FlatButton(
+                        child: Text(
+                          "Ligar",
+                          style: TextStyle(
+                            color: Colors.red,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: FlatButton(
+                        child: Text(
+                          "Editar",
+                          style: TextStyle(
+                            color: Colors.red,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                          _showContatoPage(contato: contatos[index]);
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: FlatButton(
+                        child: Text(
+                          "Excluir",
+                          style: TextStyle(
+                            color: Colors.red,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                        onPressed: () {
+                          helper.deleteContato(contatos[index].id);
+                          setState(() {
+                            contatos.removeAt(index);
+                            Navigator.pop(context);
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        });
+  }
+
+  void _showContatoPage({Contato contato}) async {
+    final recContato = await Navigator.push(context,
+        MaterialPageRoute(builder: (context) => ContatoPage(contato: contato)));
+    if (recContato != null) {
+      if (contato != null) {
         await helper.updateContato(recContato);
         _getAllContatos();
-      }else{
+      } else {
         await helper.saveContato(recContato);
       }
       _getAllContatos();
     }
   }
-  void _getAllContatos(){
-    helper.getAllContatos().then((list){
+
+  void _getAllContatos() {
+    helper.getAllContatos().then((list) {
       setState(() {
         contatos = list;
       });
     });
   }
 }
-
